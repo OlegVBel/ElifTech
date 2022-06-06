78d25f4b29b366148582dcf8adbd71b9777ea11b
OlegVBel <olegbelous2002@gmail.com>
2022-06-06 20:41:27 +0300

    Version 2. Here was fixed errors in tasks and updated functions for them

--- Back_Lesson1/script.js
+++ Back_Lesson1/script.js
@@ -1,19 +1,25 @@
 const stopBtn = document.querySelector(".stop-btn");
 const task1Btn = document.querySelector(".task1-btn");
 const task2Btn = document.querySelector(".task2-btn");
-//let id;
+
+const startCounter = 1; // for both tasks
+const startDelay = 100; // for task 2
+
 task1Btn.addEventListener("click", () => {
-  task1(1);
+  task1(startCounter);
 });
 
 task2Btn.addEventListener("click", () => {
-  task2(1, 100);
+  task2(startCounter, startDelay);
 });
 
 // Task 1
 function task1(counter, id) {
   console.log(`Hello World ${counter}`);
-  id = setTimeout(task1, 1000, ++counter, id);
+  console.log(`Delay is ${counter} ${counter === 1 ? "second" : "seconds"}`);
+  counter++;
+
+  id = setTimeout(task1, 1000 * counter, counter, id);
   if (id !== -1) {
     stopBtn.addEventListener("click", () => {
       clearTimeout(id);
@@ -24,20 +30,15 @@ function task1(counter, id) {
 // Task 2
 function task2(counter, delay, id) {
   console.log(`Hello World ${counter}`);
-  counter++;
-  console.log(`Hello World ${counter}`);
-  counter++;
-  console.log(`Hello World ${counter}`);
-  counter++;
-  console.log(`Hello World ${counter}`);
-  counter++;
-  console.log(`Hello World ${counter}`);
-  console.log(`Delay is ${delay}`);
-  id = setTimeout(task2, delay, ++counter, delay + 100, id);
-  if (id !== -1) {
-    stopBtn.addEventListener("click", () => {
-      clearTimeout(id);
-    });
+
+  if (counter % 5 === 0) {
+    console.log(`Delay was ${delay} miliseconds`);
+    delay += 100;
   }
+
+  id = setTimeout(task2, delay, ++counter, delay, id);
+  stopBtn.addEventListener("click", () => {
+    clearTimeout(id);
+  });
 }
 
